c784a021a6a09a72e81c26e2c0b6846518a06154
Nicolas Dondo <nicodondo1980@gmail.com>
2023-12-05 19:41:05 -0300

    Cambios

--- project/cliente/views.py
+++ project/cliente/views.py
@@ -58,7 +58,7 @@ def crear(request):
         form = forms.ClienteForm(request.POST)
         if form.is_valid():
             form.save()
-
+            # Redirige a la vista principal en la aplicación 'core'
             return redirect(reverse("core:index"))
     else:
         form = forms.ClienteForm()
