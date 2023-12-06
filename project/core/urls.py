856e8a2f333c4a27fcd6cbbfc5737d5f6cf59ae0
Nicolas Dondo <nicodondo1980@gmail.com>
2023-12-05 23:31:52 -0300

    Ruteo

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,3 +1,4 @@
+# core/urls.py
 from django.urls import path
 
 from . import views
@@ -7,5 +8,6 @@ app_name = "core"
 urlpatterns = [
     path("", views.home, name="index"),
     path("about/", views.about, name="about"),
-    path("cliente/", views.cliente, name="cliente")
+    path("search/", views.search, name="search"),  # Nueva ruta para la búsqueda
+    path("cliente/", views.cliente, name="cliente"),
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -10,3 +10,19 @@ def about(request):
 
 def cliente(request):
     return render (request, "cliente/crear.html")
+def obtener_resultados_segun_busqueda(query):
+    # Lógica de búsqueda simple para coincidencia de subcadenas en nombres
+    bateristas = [
+        {"nombre": "Aquiles Priester", "imagen": "aquiles-priester-drums.jpg", "descripcion": "Descripción..."},
+        {"nombre": "Lars Ulrich", "imagen": "descarga.jpg", "descripcion": "Descripción..."},
+        {"nombre": "Ray Luzier", "imagen": "korn.jpg", "descripcion": "Descripción..."},
+       
+    ]
+
+    resultados = [baterista for baterista in bateristas if query.lower() in baterista['nombre'].lower()]
+    return resultados
+
+def search(request):
+    query = request.GET.get('q', '')
+    results = obtener_resultados_segun_busqueda(query)
+    return render(request, 'core/about.html', {'query': query, 'results': results})
