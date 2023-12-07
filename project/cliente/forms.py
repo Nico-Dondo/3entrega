2220cc87aecaf378a8f2a23f32890d0202a06d2a
Nicolas Dondo <nicodondo1980@gmail.com>
2023-12-07 19:26:51 -0300

    Correccion de rutas

--- project/cliente/forms.py
+++ project/cliente/forms.py
@@ -6,4 +6,11 @@ from . import models
 class ClienteForm(forms.ModelForm):
     class Meta:
         model = models.Cliente
-        fields = ["nombre", "apellido", "nacimiento", "pais_origen"]
+        fields = ['nombre', 'apellido', 'nacimiento', 'pais_origen']
+        labels = {'nombre': 'nombre', 'apellido': 'apellido', 'nacimiento': 'nacimiento','pais_origen': 'pais_origen'}
+        widgets ={
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control'}),
+            'nacimiento': forms.TextInput(attrs={'class': 'form-control'}),
+            'pais_origen': forms.TextInput(attrs={'class': 'form-control'})
+            }
